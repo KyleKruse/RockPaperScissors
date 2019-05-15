11fa2b0d835bb307205e80a2e9d74ab5c100717b
lmarchesani <47092380+lmarchesani@users.noreply.github.com>
2019-05-15 12:11:06 -0500

    Update getChoice.py
    getChoice assigns the user choice an int value
    compare_choices returns an integer based on the two values passed in and returns a string with Victory, loss, or draw

--- getChoice.py
+++ getChoice.py
@@ -13,3 +13,33 @@ def getChoice():
         getChoice()
     
     return Int_userChoice
+
+def compare_choices(npcChoice, userChoice):
+    #rock logic
+    if userChoice == 1:
+        if npcChoice == 1:
+            result = 'Draw'
+        elif npcChoice == 2:
+            result = 'Loss'
+        else:
+            result = 'Victory'
+    #paper logic
+    elif userChoice == 2:
+        if npcChoice == 1:
+            result = 'Victory!'
+        elif npcChoice == 2:
+            result = 'Draw'
+        else:
+            result = 'Loss'
+    #scissors logic
+    elif userChoice == 3:
+        if npcChoice == 1:
+            result = 'Loss'
+        elif npcChoice == 2:
+            result = 'Victory!'
+        else:
+            result = 'Draw'
+
+
+
+    return result
